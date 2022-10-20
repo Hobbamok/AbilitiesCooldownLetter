d4631b9132ff59b5ac72ddea40a576de5b400f58
Kilian Bosse <kilian.bosse@tum.de>
2022-10-20 16:21:11 +0200

    Added the same notifications for Production and Research Specialist's abilities

--- Source/AbilitiesCooldown/AbilitiesCooldown/NotificationPatch.cs
+++ Source/AbilitiesCooldown/AbilitiesCooldown/NotificationPatch.cs
@@ -28,6 +28,45 @@ namespace AbilitiesCooldown
 
 
             }
+            if (__instance.def.defName == "ProductionCommand" && (int)cooldownTicks.GetValue(__instance) == 1)
+            {
+                //Messages.Message("Your Cleric can cast abilities again.", __instance.pawn, MessageTypeDefOf.PositiveEvent, false);
+                //alert.Notify_Started();
+                Letter letter = new StandardLetter();
+                LetterDef letDef = new LetterDef();
+                letDef.arriveSound = SoundDef.Named("");
+                letter.def = letDef;
+                letter.label = "Sigma Male Grindset time";
+                
+                
+                Find.LetterStack.ReceiveLetter(letter);
+
+
+            }
+            if (__instance.def.defName == "ResearchCommand" && (int)cooldownTicks.GetValue(__instance) == 1)
+            {
+                //Messages.Message("Your Cleric can cast abilities again.", __instance.pawn, MessageTypeDefOf.PositiveEvent, false);
+                //alert.Notify_Started();
+                Letter letter = new StandardLetter();
+                LetterDef letDef = new LetterDef();
+                letDef.arriveSound = SoundDef.Named("");
+                letter.def = letDef;
+                letter.label = "Oh yeah it's big brain time";
+                
+                
+                Find.LetterStack.ReceiveLetter(letter);
+
+
+            }
+
+            /*if (__instance.def.defName != "Burden" && __instance.def.defName != "EntropyDump" && __instance.def.defName != "WordOfTrust" && __instance.def.defName != "Painblock" && __instance.def.defName != "ChaosSkip" && (int)cooldownTicks.GetValue(__instance) == 1)
+            {
+                Messages.Message(__instance.def.defName, __instance.pawn, MessageTypeDefOf.PositiveEvent, false);
+            }*/
+            
+
+            
+
         }
         /*[HarmonyPostfix
         public static void ]*/
